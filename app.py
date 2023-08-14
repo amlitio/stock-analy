bed2f8327cebb6d0191d8c70abebf4258faa3266
kleos <36254493+amlitio@users.noreply.github.com>
2023-08-14 08:44:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import requests
+import requests 
 from bs4 import BeautifulSoup
 import pandas as pd
 from statsmodels.tsa.arima_model import ARIMA
@@ -7,68 +7,63 @@ import openai
 import io
 
 # Sentiment analysis
-@st.cache(allow_output_mutation=True)  
+@st.cache(allow_output_mutation=True)
 def analyze_sentiment(symbol):
-    try:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"Analyze the sentiment for {symbol} stock based on recent news and social media posts.",
-            max_tokens=50
-        )
-        return response.choices[0].text.strip()
-    except Exception as e:
-        st.error(f"Error analyzing sentiment: {str(e)}")
-        return None
+  try:
+    response = openai.Completion.create(
+      engine="text-davinci-003",
+      prompt=f"Analyze the sentiment for {symbol} stock based on recent news and social media posts.",  
+      max_tokens=50
+    )
+    return response.choices[0].text.strip()
+  except Exception as e:
+    return None
 
-# Stock data API
+# Stock data API 
 @st.cache
 def get_stock_data(symbol):
-    try:
-        api_key = "YOUR_API_KEY" 
-        url = f"https://financialmodelingprep.com/api/v3/historical-price-full/{symbol}"
-        response = requests.get(url, params={"apikey": api_key})
-        data = response.json()
-        df = pd.DataFrame(data['historical'])
-        return df
-    except Exception as e:
-        st.error(f"Error fetching stock data: {str(e)}")
-        return None
-
-# Inflation web scraping
+  try:
+    api_key = "YOUR_API_KEY" 
+    url = f"https://financialmodelingprep.com/api/v3/historical-price-full/{symbol}"
+    response = requests.get(url, params={"apikey": api_key}) 
+    data = response.json()
+    df = pd.DataFrame(data['historical'])
+    return df
+  except Exception as e:
+    return None
+  
+# Inflation data  
 @st.cache
 def get_inflation_data():
-    try:
-        url = "https://www.bls.gov/cpi/tables/supplemental-files/historical-cpi-u-202301.csv"
-        response = requests.get(url)
-        df = pd.read_csv(io.StringIO(response.text))
-        return df 
-    except Exception as e:
-        st.error(f"Error fetching inflation data: {str(e)}")
-        return None
+  try:
+    url = "https://www.bls.gov/cpi/tables/supplemental-files/historical-cpi-u-202301.csv"
+    response = requests.get(url)
+    df = pd.read_csv(io.StringIO(response.text))
+    return df
+  except Exception as e:
+    return None
 
-# GDP web scraping
-@st.cache
+# GDP data
+@st.cache  
 def scrape_gdp():
-    try:
-        url = "https://www.bea.gov/news/schedule"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        gdp = soup.find('span', {'class': 'chart-output'}).text
-        return float(gdp)
-    except Exception as e:
-        st.error(f"Error fetching GDP data: {str(e)}")
-        return None
-
-# Forecasting 
+  try:
+    url = "https://www.bea.gov/news/schedule"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    gdp = soup.find('span', {'class': 'chart-output'}).text
+    return float(gdp)
+  except Exception as e:
+    return None
+  
+# Forecasting
 def forecast_trends(df):
-    try:
-        model = ARIMA(df['close'], order=(5,1,0)) 
-        model_fit = model.fit(disp=0)
-        forecast = model_fit.forecast(steps=30)
-        return forecast
-    except Exception as e:
-        st.error(f"Error forecasting trends: {str(e)}")
-        return None
+  try:
+    model = ARIMA(df['close'], order=(5,1,0))
+    model_fit = model.fit(disp=0)
+    forecast = model_fit.forecast(steps=30) 
+    return forecast
+  except Exception as e:
+    return None
 
 # Streamlit app
 st.title('Financial Data Explorer')
@@ -76,26 +71,48 @@ st.title('Financial Data Explorer')
 option = st.sidebar.selectbox('Choose data', ['Stock', 'Inflation', 'GDP'])
 
 if option == 'Stock':
-    symbol = st.text_input('Enter stock symbol')
-    if symbol:
-        df = get_stock_data(symbol)
-        sentiment = analyze_sentiment(symbol)
-        if df is not None and sentiment is not None:
-            st.write('Sentiment:', sentiment)
-            st.line_chart(df['close'])
+
+  symbol = st.text_input('Enter stock symbol')
+  
+  if symbol:
+    df = get_stock_data(symbol)
+    
+    if df is None:
+      st.error("Error fetching stock data")
+      
+    else:
+      sentiment = analyze_sentiment(symbol)
+      
+      if sentiment is None:
+        st.error("Error analyzing sentiment")
         
-elif option == 'Inflation':
-    df = get_inflation_data()
-    if df is not None:
-        forecast = forecast_trends(df)
-        st.line_chart(df['CPIAUCSL'])
-        st.line_chart(forecast)
+      else:
+        st.write(sentiment)
+        st.line_chart(df['close'])
+      
+ elif option == 'Inflation':
+
+  df = get_inflation_data()
+  
+  if df is None:
+    st.error("Error fetching inflation data")
     
-else:
-    gdp = scrape_gdp()
-    if gdp is not None:
-        st.metric('GDP', gdp)
+  else:
+    forecast = forecast_trends(df)
+    st.line_chart(df['CPIAUCSL'])
+    st.line_chart(forecast)
     
+ else:
+
+  gdp = scrape_gdp()
+  
+  if gdp is None:
+    st.error("Error fetching GDP data")
+    
+  else:  
+    st.metric('GDP', gdp)
+      
+#stramlit front
 st.header('Ask a finance question')
 question = st.text_input('Enter question')
 if question:
